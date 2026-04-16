4785750d8f634f90984259c07b6386e2b8c8293e
unknown <itthiphon.jab@dome.tu.ac.th>
2026-04-16 18:36:01 +0700

    add Feature Query event data with an Active status to send to the map page.

--- src/routes/report.route.ts
+++ src/routes/report.route.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { uploadReportPic, postReport } from '../controllers/report.controller';
+import { uploadReportEvidence, postReport } from '../controllers/report.controller';
+import * as LocationController from '../controllers/location.controller';
 import { upload } from '../middlewares/upload.middleware';
 import { verifytoken } from '../middlewares/authMiddleware'; 
 
@@ -7,8 +8,10 @@ const router = Router();
 
 // ใส่ upload.single('image') เพื่อบอกว่าชื่อฟิลด์ในฟอร์มคือ 'image'
 // เพิ่ม verifytoken เพื่อป้องกันไม่ให้คนนอกอัปโหลดไฟล์ขยะเข้ามาได้
-router.post('/postpic', verifytoken, upload.single('image'), uploadReportPic);
+router.post('/postpic', verifytoken, upload.single('image'), uploadReportEvidence);
 router.post('/post', verifytoken, postReport);
+
+router.get('/active-map', LocationController.getActiveIncidentPoints);
 // 2. User ส่งข้อมูลรายงาน (พิกัด + รายละเอียด + URL รูป)
 
 export default router;
