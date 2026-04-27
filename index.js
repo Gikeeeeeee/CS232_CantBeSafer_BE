a5a9e617ab281bf94cf773b44366cc1db3387eb9
Gikeeeeeee <tatchakrit.gike@gmail.com>
2026-04-28 02:33:18 +0700

    feat: initialize Express application and add serverless-http support for deployment

---
+++ index.js
@@ -0,0 +1,5 @@
+// index.js
+const serverless = require('serverless-http');
+const app = require('./dist/index.js').default; 
+
+module.exports.handler = serverless(app);
--- src/index.ts
+++ src/index.ts
@@ -59,3 +59,5 @@ const PORT = process.env.WEB_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
